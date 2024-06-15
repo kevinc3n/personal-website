62add18d1755b889cfb16939033dad36b2f51117
kevinc3n <kcen@ufl.edu>
2024-06-15 15:12:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi bruh.
         </p>
         <a
           className="App-link"
